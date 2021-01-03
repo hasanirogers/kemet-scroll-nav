e470b2b68ac39aa93dc04733224f173754f96c47
Hasani Rogers <hasani.rogers@gmail.com>
2021-01-03 15:56:46 -0500

    added tests

--- test/kemet-scroll-nav.test.js
+++ test/kemet-scroll-nav.test.js
@@ -3,24 +3,12 @@ import { html, fixture, expect } from '@open-wc/testing';
 import '../kemet-scroll-nav.js';
 
 describe('KemetScrollNav', () => {
-  it('has a default title "Hey there" and counter 5', async () => {
-    const el = await fixture(html`<kemet-scroll-nav></kemet-scroll-nav>`);
+  it('has the correct defaults', async () => {
+    const element = await fixture(html`<kemet-scroll-nav></kemet-scroll-nav>`);
 
-    expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
-  });
-
-  it('increases the counter on button click', async () => {
-    const el = await fixture(html`<kemet-scroll-nav></kemet-scroll-nav>`);
-    el.shadowRoot.querySelector('button').click();
-
-    expect(el.counter).to.equal(6);
-  });
-
-  it('can override the title via attribute', async () => {
-    const el = await fixture(html`<kemet-scroll-nav title="attribute title"></kemet-scroll-nav>`);
-
-    expect(el.title).to.equal('attribute title');
+    expect(element.effect).to.equal('sticky');
+    expect(element.transform).to.equal(false);
+    expect(element.offset).to.equal(0);
   });
 
   it('passes the a11y audit', async () => {
